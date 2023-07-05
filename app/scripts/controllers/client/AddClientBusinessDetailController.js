79985dce43c4e948b61a60fbb95f8666180ad917
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2023-07-05 14:38:37 +0300

    add route params on clientId

--- app/scripts/controllers/client/AddClientBusinessDetailController.js
+++ app/scripts/controllers/client/AddClientBusinessDetailController.js
@@ -1,11 +1,13 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        AddClientBusinessDetailController: function (scope, $rootScope, resourceFactory, location, dateFilter,WizardHandler, translate,routeParams) {
-            scope.restrictDate = new Date();
+        AddClientBusinessDetailController: function (scope, resourceFactory, location, dateFilter,WizardHandler, translate,routeParams) {
             scope.formData = {};
             scope.showOrHideValue = "show";
-            scope.date = {};
+            scope.first = {};
+            scope.first.businessCreationDate = new Date();
             scope.clientId = routeParams.clientId;
+             scope.businessDetails = {};
+
 
             resourceFactory.clientBusinessDetailTemplate.get({clientId:scope.clientId},function (data) {
                 scope.clientbusinessDetails = data;
@@ -17,7 +19,9 @@
                 if (data.clientAccount) {
                     scope.clientName = data.clientAccount.displayName;
                 }
-
+                if (scope.first.businessCreationDate) {
+                    this.formData.businessCreationDate = dateFilter(scope.first.businessCreationDate, scope.df);
+                }
                 scope.businessDetails = angular.copy(scope.formData);
                 scope.isClicked = false;
 
@@ -29,7 +33,7 @@
                 scope.businessDetails = angular.extend(scope.businessDetails,newVal);
              },true);
 
-             $rootScope.formValue = function(array,model,findattr,retAttr){
+             scope.formValue = function(array,model,findattr,retAttr){
                  findattr = findattr ? findattr : 'id';
                  retAttr = retAttr ? retAttr : 'value';
                  console.log(findattr,retAttr,model);
@@ -49,8 +53,12 @@
 
 
             scope.submit = function () {
-                var reqFirstDate = dateFilter(scope.date.first, scope.df);
-                var reqSecondDate = dateFilter(scope.date.second, scope.df);
+                 this.formData.locale = scope.optlang.code;
+                 this.formData.dateFormat = scope.df;
+
+                 if (scope.first.businessCreationDate) {
+                        this.formData.businessCreationDate = dateFilter(scope.first.businessCreationDate, scope.df);
+                    }
                 resourceFactory.clientBusinessDetailResource.save({clientId:scope.clientId},this.formData, function (data) {
                     location.path('/viewClientBusinessDetails/' + data.resourceId);
                 });
@@ -59,7 +67,7 @@
 
         }
     });
-    mifosX.ng.application.controller('AddClientBusinessDetailController', ['$scope','$rootScope', 'ResourceFactory', '$location', 'dateFilter','WizardHandler', '$translate','$routeParams', mifosX.controllers.AddClientBusinessDetailController]).run(function ($log) {
+    mifosX.ng.application.controller('AddClientBusinessDetailController', ['$scope', 'ResourceFactory', '$location', 'dateFilter','WizardHandler', '$translate','$routeParams', mifosX.controllers.AddClientBusinessDetailController]).run(function ($log) {
         $log.info("AddClientBusinessDetailController initialized");
     });
 }(mifosX.controllers || {}));
