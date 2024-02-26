01ecd4df9dfc146033669747d355fe1693f25831
Faheem Ahmad <faheemahmad@sanmar.com>
2024-02-26 22:21:41 +0500

    INKO-371

--- app/scripts/directives/NumberOnlyDirective.js
+++ app/scripts/directives/NumberOnlyDirective.js
@@ -6,7 +6,7 @@
                     link: function (scope, element, attr, ngModelCtrl) {
                         function fromUser(text) {
                             if (text) {
-                                var transformedInput = text.replace(/[^0-9]/g, '');
+                                var transformedInput = text.replace(/[^0-9.]/g, '');
 
                                 if (transformedInput !== text) {
                                     ngModelCtrl.$setViewValue(transformedInput);
